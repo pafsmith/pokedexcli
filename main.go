88046517810f347db64c65edb6f631be82c93c52
Paul Smith <psmith@magna.co.uk>
2025-10-24 09:26:16 +0100

    Added repl commands

--- main.go
+++ main.go
@@ -19,9 +19,45 @@ func cleanInput(text string) []string {
 }
 
 
+func commandExit() error {
+		fmt.Println("Closing the Pokedex... Goodbye!")
+		os.Exit(0)
+		return nil
+	}
+
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:\n\n")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+
+
+
 func main() {
-	
-	
+	commands := map[string]cliCommand{
+    "exit": {
+        name:        "exit",
+        description: "Exit the Pokedex",
+        callback:    commandExit,
+    },
+		"help": {
+			name: "help",
+			description: "Shows usage guide",
+			callback: commandHelp,
+		},
+}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
@@ -32,6 +68,16 @@ func main() {
 		}
 		inputText := scanner.Text()
 		cleanedInputText := cleanInput(inputText)
-		fmt.Printf("Your command was: %s\n", cleanedInputText[0])
+		command, ok := commands[cleanedInputText[0]]
+		if !ok {
+			fmt.Println("Unknown command.")
+			continue
+		}
+		err = command.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+		
+
 	}
 }
